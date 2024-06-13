feb2624308a40115aa5b7cdd0921787daafd8a27
Frank Anin <frankanin99@gmail.com>
2024-06-13 14:57:17 +0000

    web framework

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""List of states"""
+from flask import Flask, render_template
+from models import *
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states sorted by name"""
+    states = sorted(list(storage.all("State").values()),
+                    key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
